33b4e94c495096c38863f2975805945ed8234ddb
KelviPascoal <kelvi_ugo@hotmail.com>
2025-04-25 12:25:43 -0300

    feat: adiciona login ao projeto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import Menu from "@/app/components/menu";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Menu />
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "@/app/components/login";
+
+export default async function LoginPage() {
+  return (
+    <main>
+      <h1>login</h1>
+      <LoginForm />
+    </main>
+  );
+}
